e8a7070eaaf91187ea5e8ea0989fad5f80eb2412
mathieu salles <mathieusalles@MacBook-Pro.local>
2021-04-18 20:16:27 +0200

    feat(d3): add error boundaries component

--- src/d3/d3Dashboard.jsx
+++ src/d3/d3Dashboard.jsx
@@ -1,28 +1,40 @@
 import React from 'react'
-// import {arc} from 'd3-shape'
+import {ErrorBoundary} from 'react-error-boundary'
+import * as d3 from "d3";
 import './styles/d3Dashboard.scss'
 
 const D3Dashboard = () => {
-  // const arcPathGenerator = arc()
-  //   .innerRadius(18)
-  //   .outerRadius(40)
-  //   .startAngle(0)
-  //   .endAngle(5.5)
-  //   .padAngle(0)
-  //   .cornerRadius(20)
-  // const arcPath = arcPathGenerator()
-
+  function ErrorFallback({error, resetErrorBoundary}) {
+    return (
+      <div role="alert">
+        <p>Something went wrong:</p>
+        <pre>{error.message}</pre>
+        <button onClick={resetErrorBoundary}>Try again</button>
+      </div>
+    )
+  }
+  const arcPathGenerator = d3.arc()
+    .innerRadius(18)
+    .outerRadius(40)
+    .startAngle(0)
+    .endAngle(5.5)
+    .padAngle(0)
+    .cornerRadius(20)
+  const arcPath = arcPathGenerator()
   return (
-    <div className="d3Dashboard-Container">
-      <h4>first title</h4>
-      {/*<svg width="100" height="100">*/}
-      {/*  <path*/}
-      {/*    fill="cornflowerblue"*/}
-      {/*    d={arcPath}*/}
-      {/*    style="transform: translate(50%, 50%)"*/}
-      {/*  />*/}
-      {/*</svg>*/}
-    </div>
+    <ErrorBoundary
+      FallbackComponent={ErrorFallback}
+    >
+      <div className="D3Dashboard-Container">
+        <svg width="100" height="100">
+          <path
+            fill="cornflowerblue"
+            d={arcPath}
+            style={{"transform": "translate(50%, 50%)"}}
+          />
+        </svg>
+      </div>
+    </ErrorBoundary>
   )
 }
 
