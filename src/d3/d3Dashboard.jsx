79f2e1e9d75c9030457c53afc0b3e806809c02af
Mathieu Salles <mathieu.sal@gmail.com>
2021-05-14 17:08:09 +0200

    feat(D3): new component -> color gradient

--- src/d3/d3Dashboard.jsx
+++ src/d3/d3Dashboard.jsx
@@ -5,6 +5,7 @@ import Transitions from 'Src/d3/components/transitions/transitions'
 import BarChart from 'Src/d3/components/barChart/barChart'
 import GaugeChart from 'Src/d3/components/gaugeChart/gaugeChart'
 import Viewbox from 'Src/d3/components/viewbox/viewbox'
+import ColorGradient from 'Src/d3/components/colorGradient/colorGradient'
 import BarChartCustom from 'Src/d3/components/barChartCustom/barChartCustom'
 import LineChart from 'Src/d3/components/lineChart/lineChart'
 import './styles/d3Dashboard.scss'
@@ -37,6 +38,7 @@ const D3Dashboard = () => {
         </div>
         <div className="row">
           <LineChart />
+          <ColorGradient />
         </div>
       </div>
     </ErrorBoundary>
