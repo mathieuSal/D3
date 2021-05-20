84dbe611feda95110c5e87e76473e1a4b7ff2111
Mathieu Salles <mathieu.sal@gmail.com>
2021-05-20 21:59:18 +0200

    feat(D3): init multiple chart

---
+++ src/d3/components/multipleCharts/multipleCharts.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './styles/multipleCharts.scss'
+
+const MultipleCharts = () => {
+  return (
+    <div className="D3Dashboard-MultipleChart-Container">
+
+    </div>
+  )
+}
+
+export default MultipleCharts
